cce22211549fc87deac049589e568a3708cba442
Antonio Pardo <apardo@spluca.org>
2026-02-09 14:20:58 +0100

    cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.mikrom.es',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+  }),
+});
